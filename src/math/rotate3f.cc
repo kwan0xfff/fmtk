8b9940ff8f1af3fc6a9dcc6cd6abc7ecfbbd0b98
R. J. Dev. Kwan <kwan0xfff@gmail.com>
2014-09-07 19:25:50 -0700

    Fix bad signage.

--- src/math/rotate3f.cc
+++ src/math/rotate3f.cc
@@ -26,8 +26,8 @@ Cart3f rotateX(float angle, Cart3f vec)
     cos_a = cosf(angle);
 
     v1[0] =         v0[0];
-    v1[1] =                 cos_a*v0[1] - sin_a*v0[2];
-    v1[2] =                 sin_a*v0[1] + cos_a*v0[2];
+    v1[1] =                 cos_a*v0[1] + sin_a*v0[2];
+    v1[2] =               - sin_a*v0[1] + cos_a*v0[2];
 
     return v1;
 }
@@ -42,9 +42,9 @@ Cart3f rotateY(float angle, Cart3f vec)
     sin_a = sinf(angle);
     cos_a = cosf(angle);
 
-    v1[0] =   cos_a*v0[0]               + sin_a*v0[2];
+    v1[0] =   cos_a*v0[0]               - sin_a*v0[2];
     v1[1] =                       v0[1];
-    v1[2] = - sin_a*v0[0]               + cos_a*v0[2];
+    v1[2] =   sin_a*v0[0]               + cos_a*v0[2];
 
     return v1;
 }
@@ -59,8 +59,8 @@ Cart3f rotateZ(float angle, Cart3f vec)
     sin_a = sin(angle);
     cos_a = cos(angle);
 
-    v1[0] =   cos_a*v0[0] - sin_a*v0[1];
-    v1[1] =   sin_a*v0[0] + cos_a*v0[1];
+    v1[0] =   cos_a*v0[0] + sin_a*v0[1];
+    v1[1] = - sin_a*v0[0] + cos_a*v0[1];
     v1[2] =                                     v0[2];
 
     return v1;
