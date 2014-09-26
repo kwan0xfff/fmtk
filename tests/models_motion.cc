5b8f883f89b7d8a6522ca04e2c3405300ec572a4
R. J. Dev. Kwan <kwan0xfff@gmail.com>
2014-09-25 23:43:48 -0700

    Add standard const and optional diagnostic prints for motion test.

--- tests/models_motion.cc
+++ tests/models_motion.cc
@@ -7,6 +7,24 @@
 
 #include "models/SimpleMotion.h"
 
+#if 0
+void print_cart3f(Cart3f& vec)
+{
+    printf("%4.2f %4.2f %4.2f\n", vec[0], vec[1], vec[2]);
+}
+
+void print_cart3f(const Cart3f& vec)
+{
+    Cart3f v = vec;
+    print_cart3f(v);
+}
+#endif //0
+
+const Cart3f z_1e3 = Cart3f(0.0, 0.0, 1000.0);
+const Cart3f z_1e2 = Cart3f(0.0, 0.0,  100.0);
+const Cart3f z_1e1 = Cart3f(0.0, 0.0,   10.0);
+const Cart3f z_1e0 = Cart3f(0.0, 0.0,    1.0);
+const Cart3f zero  = Cart3f(0.0, 0.0,    0.0);
 
 // epsilon uncerainty -- 10 to minus X power
 float epsM[] = { 1, 1.0e-1, 1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6 };
@@ -51,17 +69,21 @@ TEST(Models, MotionSimp1)
     Motion motion;
     SimpleMotionState ms;
 
-    ms.x = Cart3f(0.0, 0.0, 1000.0);
-    ms.dx = Cart3f(0.0, 0.0, 100.0);
-    ms.ddx = Cart3f(0.0, 0.0, 0.0);
+    ms.x = z_1e3;
+    ms.dx = z_1e2;
+    ms.ddx = zero;
     ms.dt = 0.2;        // step-size
+    //print_cart3f(ms.dx);
     motion.setState(ms);
 
     for (int i=0; i<10; ++i) {
         motion.step();
+        ms = motion.getState();
+        //print_cart3f(ms.x);
     }
     ms = motion.getState();
 
     EXPECT_LE(ms.x[0], 0.0);
     EXPECT_LE(fabs(ms.x[2]-1200.0), epsM[5]);
 }
+
